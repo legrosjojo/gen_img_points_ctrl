73635948f515b437d63df0c3e65ff9cf8fe57f56
GUILLAUME Joffrey <guillaume.joffrey67@gmail.com>
2025-01-29 08:57:19 +0100

    doxypypy

--- code.py
+++ code.py
@@ -1,11 +1,29 @@
 import cv2 as cv
 import sys
 
-img_path = "mire_315a.png"
-img_orig = cv.imread(cv.samples.findFile(img_path))
+'''
+This function open and show an img
+Args:
+    img_path: path to an image
 
-if img_orig is None:
-    sys.exit("err::Could not read the image.")
+Returns:
+    An opened image
 
-cv.imshow("Display window", img_orig)
-key = cv.waitKey(0)
+Exemples:
+    >>>openShowImg("mire_315a.png")
+'''
+def openShowImg(img_path):
+    #img_path = "mire_315a.png"
+    if img_path is None:
+        sys.exit("err::Could not read the image.")
+    
+    img_orig = cv.imread(cv.samples.findFile(img_path))
+    
+    cv.imshow("Display window", img_orig)
+    key = cv.waitKey(0)
+    return img_orig
+
+#if key == ord('s'):                        si k est tapé
+#   cv.imwrite("img_modif.png", img_modif)  alors enregistre img_modif en img_modif.png
+
+openShowImg("mire_315a.png")
